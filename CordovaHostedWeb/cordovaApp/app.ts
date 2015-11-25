aedb0418f427a37de1f450286ec8b14e3cb16b91
Mikejo5001 <m.jones168@outlook.com>
2015-11-25 11:01:38 -0800

    removing files

--- CordovaHostedWeb/cordovaApp/app.ts
+++ CordovaHostedWeb/cordovaApp/app.ts
@@ -11,38 +11,7 @@ module CordovaHostedApp {
         function onDeviceReady() {
             // Handle the Cordova pause and resume events
             document.addEventListener('pause', onPause, false);
-            document.addEventListener('resume', onResume, false);
-
-            document.getElementsByClassName('btn-lg')[0].addEventListener('click', takePicture);
-            
-        }
-
-        function takePicture() {
-            if (!navigator.camera) {
-                alert("Camera API not supported");
-                return;
-            }
-            var options = {
-                quality: 20,
-                destinationType: Camera.DestinationType.DATA_URL,
-                sourceType: 1,      
-                encodingType: 0     
-            };
-
-            navigator.camera.getPicture(
-                function (imgData) {
-                    var el : HTMLElement;
-                    el = <HTMLElement>document.getElementsByClassName('media-object')[0];
-                    var srcAttr = document.createAttribute("src");
-                    srcAttr.value = "data:image/jpeg;base64," + imgData;
-                    el.attributes.setNamedItem(srcAttr);                    
-                },
-                function () {
-                    alert('Error taking picture');
-                },
-                options);
-
-            return false;
+            document.addEventListener('resume', onResume, false);      
         }
 
         function onPause() {
